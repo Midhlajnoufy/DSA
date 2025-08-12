d17774b511dd2d897ad18adaddf3ec2dd23ca3b7
mec <mec@cc-2-8.mec.ac.in>
2025-08-12 16:30:24 +0530

    some changes

--- cycle5/infitopost.c
+++ cycle5/infitopost.c
@@ -1,2 +1,17 @@
 #include<stdio.h>
-
+#include<ctype.h>
+//main function
+int main(){
+	int size;
+	printf("size of string to be converted into postfix: ");
+	scanf("%d",&size);
+	char string[size];
+	char out[size];
+	printf("enter the infix expression as string: ");
+	fgets(string,size,stdin);
+	int i=0;
+	while(string[i]!='\0'){
+		if(isalnum(string[i]){
+			out[i]=string[i];
+		}else{
+			if(prec(string[i])>prec(stack[top]) )
